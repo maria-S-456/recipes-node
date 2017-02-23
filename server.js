3c85291e7b0c2007cd4ed23fb48d40e979009216
Maria Mohl <mars381pika@gmail.com>
2017-02-23 21:52:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -14,14 +13,36 @@ app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
 // so there's some data to look at
+/*
 Recipes.create('Baked Burritos', 'beans');
 Recipes.create('Tomato Soup', 'tomatoes');
 Recipes.create('Stuffed Peppers', 'bell peppers');
+*/
 
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
+});
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
 });
 
 app.listen(process.env.PORT || 8080, () => {
