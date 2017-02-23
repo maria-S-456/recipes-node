0a9d19976ecd47e35e255e3658b6baa5f8073a9c
Maria Mohl <mars381pika@gmail.com>
2017-02-24 00:52:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
-/*
-Recipes.create('Baked Burritos', 'beans');
-Recipes.create('Tomato Soup', 'tomatoes');
-Recipes.create('Stuffed Peppers', 'bell peppers');
-*/
-
 Recipes.create(
   'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
 Recipes.create(
@@ -29,6 +21,7 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 });
 
+//with post request, you must change the values in body in postman, after selecting post. this will create a new recipe
 app.post('/recipes', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'ingredients'];
@@ -40,9 +33,45 @@ app.post('/recipes', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
-  const item = Recipes.create(req.body.name, req.body.budget);
+//make sure it is req.body.ingredients and name
+  const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
+});
+
+
+//works correctly, but still giving list of errors
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id); 
+  console.log(`Deleted recipe item \`${req.put.id}\``);
+  res.status(204).end();
+});
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe \`${req.params.id}\``);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+//when putting, must put id number in address
+
+  res.status(200).json(updatedItem);
 });
 
 app.listen(process.env.PORT || 8080, () => {
