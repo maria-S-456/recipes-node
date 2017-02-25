3e98b5b058551feb3dc4c088d7d6181d2a3cb9b4
Maria Mohl <mars381pika@gmail.com>
2017-02-25 18:03:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,48 @@
-const express = require('express');
-const router = express.Router();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const uuid = require('uuid');
 
-const {Recipes} = require('./models');
+function StorageException(message) {
+   this.message = message;
+   this.name = "StorageException";
+}
 
-const jsonParser = bodyParser.json();
-const app = express();
-
-// log the http layer
-app.use(morgan('common'));
-
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
-// when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-});
-
-//with post request, you must change the values in body in postman, after selecting post. this will create a new recipe
-app.post('/recipes', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
+const Recipes = {
+  create: function(name, ingredients) {
+    console.log('Creating a new recipe');
+    const item = {
+      name: name,
+      id: uuid.v4(),
+      ingredients: ingredients
+    };
+    this.items[item.id] = item;
+    return item;
+  },
+  get: function() {
+    console.log('Retreiving recipes');
+    return Object.keys(this.items).map(key => this.items[key]);
+  },
+  delete: function(itemId) {
+    console.log(`Deleting recipe with id \`${itemId}\``);
+    delete this.items[itemId];
+  },
+  update: function(updatedItem) {
+    console.log(`Updating recipe with id \`${updatedItem.id}\``);
+    const {id} = updatedItem;
+    if (!(id in this.items)) {
+      throw StorageException(
+        `Can't update item \`${id}\` because doesn't exist.`)
     }
+    this.items[updatedItem.id] = updatedItem;
+    return updatedItem;
   }
-//make sure it is req.body.ingredients and name
-  const item = Recipes.create(req.body.name, req.body.ingredients);
-  res.status(201).json(item);
-});
+};
 
 
-//works correctly, but still giving list of errors
-app.delete('/recipes/:id', (req, res) => {
-  Recipes.delete(req.params.id); 
-  console.log(`Deleted recipe item \`${req.put.id}\``);
-  res.status(204).end();
-});
+function createRecipes() {
+  const storage = Object.create(Recipes);
+  storage.items = {};
+  return storage;
+}
 
-app.put('/recipes/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'ingredients', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating recipe \`${req.params.id}\``);
-  const updatedItem = Recipes.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-//when putting, must put id number in address
-
-  res.status(200).json(updatedItem);
-});
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+module.exports = {
+  Recipes: createRecipes()
+}
